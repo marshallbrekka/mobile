7dbb685410f88c30dd1d125fa4ff3e84345f4ae1
Marshall Brekka <marshallsmedia@gmail.com>
2014-01-07 19:39:14 -0800

    rewrite of navstack so that all of the show/hide logic is contained within  the rfzViewStack directive, also allows nested stacks, and scope properties are available with some info about the current stack. New tab directive that works just like ng-switch except that once a view is created it is never destroyed, just hidden

--- app/js/directive/view.js
+++ app/js/directive/view.js
@@ -46,181 +46,32 @@ and switching between them emulates the standard iOS transitions.
     <div rfz-pane-body>Content Of Notifications Pane</div>
   </div>
 </div>
-
 */
-
-
-// The rfzView controller sets the function openPane (which is
-// available to all children of the rfzView directive) which is used
-// to control which pane is shown, and what direction it should come
-// from when being shown.
-/*RFZ.controller("rfzView", function($scope) {
-  console.log("made controller");
-  $scope.value = "rfzView";
-  return {
-    cases : {},
-    openPane : function(paneName, direction) {
-      $scope.rfzViewDirection = direction;
-      $scope.rfzView = paneName;
-      if(!$scope.$$phase) $scope.$digest();
-    }
-  }
-});*/
-
-
-/**
- * Return the siblings between `startNode` and `endNode`, inclusive
- * @param {Object} object with `startNode` and `endNode` properties
- * @returns jQlite object containing the elements
- */
-function getBlockElements(block) {
-  if (block.startNode === block.endNode) {
-    return angular.element(block.startNode);
-  }
-  var element = block.startNode;
-  var elements = [element];
-  do {
-    element = element.nextSibling;
-    if (!element) break;
-    elements.push(element);
-  } while (element !== block.endNode);
-  return angular.element(elements);
-}
-
-
-RFZ.controller("rfzNavStack", function($scope) {
-  var stack = [];
-  $scope.$rfzNavState = {};
-  $scope.$rfzNavDepth = 0;
-
-  function push(viewName, enterType) {
-    stack.push({name : $scope.$rfzViewName,
-                exitType : enterType});
-    $scope.$rfzNavState[$scope.$rfzViewName] = true;
-    $scope.$rfzViewName = viewName;
-    $scope.$rfzViewTransition = enterType;
-    $scope.$rfzNavDepth++;
-  }
-
-  function pop() {
-    var previousView = stack.pop();
-    delete $scope.$rfzNavState[previousView.name];
-    $scope.$rfzViewName = previousView.name;
-    $scope.$rfzViewTransition = previousView.exitType;
-    $scope.$rfzNavDepth--;
-  }
-
-  $scope.$rfzNavPush = push;
-  $scope.$rfzNavPop = pop;
-
-  return {
-    push : push,
-    pop : pop
-  };
-});
-
-RFZ.directive("rfzView", function() {
-  return {
-    restrict : "A",
-    require : "rfzView",
-    controller : "rfzNavStack",
-    scope : true,
-    compile : function(tElement) {
-      tElement.addClass("rfz-view");
-      return {
-        pre : function(scope, element, attr, ctrl) {
-          scope.$rfzViewName = attr.rfzView;
-          scope.$watch("$rfzViewTransition", function(transitionType, previousTransitionType) {
-            element.addClass("rfz-nav-stack-" + transitionType);
-            element.removeClass("rfz-nav-stack-" + previousTransitionType);
-          });
-        }
-      };
-    }
-  }
-});
-
-  RFZ.directive("rfzPane", ["$animate", function($animate) {
-    return {
-      restrict : "A",
-      require : "^rfzView",
-      transclude : "element",
-      priority : 10000,
-      $$tlb : true,
-      compile : function(tElement) {
-        tElement.addClass("rfz-pane");
-        return {
-          pre : function(scope, element, attr, ctrl, $transclude) {
-            var childScope, block, isVisible;
-            scope.$on("$includeContentRequested", function() {
-              console.log("content requested");
-            });
-            
-            scope.$watch("$rfzNavState." + attr.rfzPane, function(val) {
-              if (block) {
-                if (val) {
-                  $animate.addClass(angular.element(block.startNode), "ng-hide");
-                } else {
-                  $animate.removeClass(angular.element(block.startNode), "ng-hide");
-                }
-              }
-            });
-
-            scope.$watch("$rfzViewName", function rfzPaneWatchAction(name, previousName) {
-              if (name === attr.rfzPane) {
-                if (!childScope) {
-                  childScope = scope.$new();
-                  console.log("pre transclude");
-                  $transclude(childScope, function(clone) {
-                    console.log("transcluded");
-                    clone.addClass("rfz-pane");
-                    clone[0].style.zIndex = new String(scope.$rfzNavDepth);
-                    block = {
-                      startNode : clone[0],
-                      endNode : clone[clone.length++] = document.createComment(" end rfzPane: " 
-                                                                               + attr.rfzPane + " ")
-                    };
-                    $animate.enter(clone, element.parent(), element);
-                    console.log("entered");
-                  });
-                }
-              } else if (previousName === attr.rfzPane && !scope.$rfzNavState[attr.rfzPane]) {
-                if (childScope) {
-                  childScope.$destroy();
-                  childScope = null;
-                }
-                if (block) {
-                  $animate.leave(getBlockElements(block));
-                  block = null;
-                }
-              }
-            });
-          }
-        }
-      }
-
-    };
-  }]);
 
   // Creates the pane header, the value of the attribute becomes the
   // title of the header. It also adds some classes for us.
   //
   // <div rfz-pane-header="Overview">
-  RFZ.directive("rfzPaneHeader", function() {
+  RFZ.directive("rfzViewHeader", function() {
     return {
       restrict : "A",
       compile : function(element, attrs) {
-        element.append(angular.element("<div class='header-name'>" + attrs.rfzPaneHeader + "</div>"))
-          .removeAttr("rfzPaneHeader")
+        element.append(angular.element("<div class='header-name'>" + attrs.rfzViewHeader + "</div>"))
+          .removeAttr("rfzViewHeader")
           .addClass("header");
+        return function(scope, element, attr) {
+          if (scope.$rfzViewProperties) {
+            scope.$rfzViewProperties.title = attrs.rfzViewHeader;
+          }
+        }
       }
     }
   });
 
-  RFZ.directive("rfzPaneHeaderButton", function() {
+  RFZ.directive("rfzViewHeaderButton", function() {
     return {
       restrict : "A",
-      require : "^rfzView",
+//      require : "^rfzView",
       compile : function(elem, attrs, link) {
         elem.attr("compiled", true);
         elem.addClass("header-button");
@@ -234,11 +85,180 @@ RFZ.directive("rfzView", function() {
   // This directive simply adds a class to its element, which is
   // neccesary for the pane change animation, where the body slides
   // but the header fades.
-  RFZ.directive("rfzPaneBody", function() {
+  RFZ.directive("rfzViewBody", function() {
     return {
       restrict : "A",
       link : function(scope, element) {
         element.addClass("rfz-pane-body view");
+      }
+    }
+  });
+
+  /*
+  When the directive is first initialized the value for
+  rfz-view-stack becomes the first view on the history stack.
+
+  With nested view stacks, if the length of the history is equal to 1
+  and the $pop method was called, then it looks for its controller on
+  its parent and calls $pop there, thus removing the current view from
+  the stack, as well as the last view on the parents stack.
+  */
+  RFZ.directive("rfzViewStack", ["$animate", function($animate) {
+    function transitionClass(type) {
+      return "rfz-view-stack-transition-" + type;
+    }
+
+    return {
+      restrict : "A",
+      controller : function() {
+        this.views = {};
+        this.history = [];
+        this.$$pop = function(){};
+        this.depthIndex = 1;
+      },
+      link : function(scope, element, attr, ctrl) {
+        var parentController = element.parent().controller("rfzViewStack");
+        if (parentController) {
+          ctrl.depthIndex = parentController.depthIndex + 1;
+        }
+        function popView() {
+          if (ctrl.history.length === 1){
+            if (parentController) {
+              parentController.$$pop();
+            } else {
+              throw new Error("rfzViewStack: tried to call $pop, but already at beginning" + 
+                              " of stack, and no parent stack exists.");
+            }
+          } else {
+            // Remove the current view, and reveal the previous one.
+            var current = ctrl.history.pop();
+            var previous = ctrl.history[ctrl.history.length - 1];
+            element.addClass(transitionClass(current.transition));
+            current.scope.$destroy();
+            $animate.removeClass(previous.element, "ng-hide");
+            $animate.leave(current.element, function() {
+              element.removeClass(transitionClass(current.transition));
+            });
+            ctrl.depthIndex--;
+          }
+        }
+
+        function pushView(name, transitionType) {
+          var viewObj = ctrl.views[name];
+          if (!viewObj) {
+            throw new Error("rfzViewStack: tried to push the view " + name +
+                            " but no view by that name exists");
+          }
+          element.addClass(transitionClass(transitionType));
+          // Properties to use for the new view
+          var newScope = scope.$new();
+          var view = {
+            scope : newScope,
+            transition : transitionType
+          };
+
+          viewObj.transclude(view.scope, function(clone) {
+            clone.addClass("rfz-pane");
+            clone.css("z-index", ctrl.depthIndex++ + "");
+            view.element = clone;
+            var anchor = viewObj.element;
+            if (ctrl.history.length === 0) {
+              newScope.$rfzViewProperties = {};
+            } else {
+              var current = ctrl.history[ctrl.history.length - 1];
+              newScope.$rfzViewProperties = {
+                canGoBack : true,
+                previousTitle : current.scope.$rfzViewProperties.title
+              };
+              $animate.addClass(current.element, "ng-hide");
+            }
+            ctrl.history.push(view);
+            $animate.enter(clone, anchor.parent(), anchor, function() {
+              element.removeClass(transitionClass(transitionType));
+            });
+          });
+        }
+
+        ctrl.$$pop = popView;
+
+        element.addClass("rfz-view");
+        var previousStack = scope.$rfzViewStack;
+        scope.$rfzViewStack = {
+          $push : function(name, transitionType) {
+            pushView(name, transitionType);
+          },
+          $pop : function() {
+            popView();
+          }
+        };
+        pushView(attr.rfzViewStack, "none");
+      }
+    }
+  }]);
+
+  RFZ.directive("rfzView", function() {
+    return {
+      transclude: 'element',
+      priority: 800,
+      require: '^rfzViewStack',
+      compile: function(element, attrs) {
+        return function(scope, element, attr, ctrl, $transclude) {
+          ctrl.views[attrs.rfzView] = {
+            transclude : $transclude,
+            element : element
+          };
+        };
+      }
+    }
+  });
+
+  RFZ.directive("rfzTabs", ["$animate", function($animate) {
+    return {
+      restrict : "A",
+      require : "rfzTabs",
+      controller : ["$scope", function() {
+        this.cases = {};
+      }],
+      link : function(scope, element, attr, ctrl) {
+        var visibleView, watchExpr = attr.rfzTabs;
+        scope.$watch(watchExpr, function(val) {
+          var nextView = ctrl.cases["!" + val];
+          if (!nextView) {
+            throw new Error("rfzTabs: no tab exists with the name " + val);
+          }
+          // If the next view has aleady been created once, just show
+          // its hidden view
+          if (nextView.clone) {
+            $animate.removeClass(nextView.clone, "ng-hide");
+            $animate.addClass(visibleView, "ng-hide");
+            visibleView = nextView.clone;
+          } else {
+            nextView.scope = scope.$new();
+            nextView.transclude(nextView.scope, function(clone) {
+              nextView.clone = clone;
+              $animate.enter(clone, nextView.element.parent(), nextView.element);
+              if (visibleView) {
+                $animate.addClass(visibleView, "ng-hide");
+              }
+              visibleView = clone;
+            });
+          }
+        });
+      }
+    }
+  }]);
+
+  RFZ.directive("rfzTab", function() {
+    return {
+      restrict : "A",
+      transclude : "element",
+      priority : 800,
+      require : "^rfzTabs",
+      link : function(scope, element, attrs, ctrl, $transclude) {
+        ctrl.cases["!" + attrs.rfzTab] = {
+          transclude : $transclude,
+          element : element
+        };
       }
     }
   });
