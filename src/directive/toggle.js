85ea09a28bf2605507cdb237a101f9aae1e95b68
Marshall Brekka <marshallsmedia@gmail.com>
2014-02-26 19:36:28 -0800

    redo the ios toggle control so that the middle is transparent, add the android toggle control

--- src/directive/toggle.js
+++ src/directive/toggle.js
@@ -1,104 +1,5 @@
-/*lib.directive("rfzToggle", ["$rfz.util.css", "$rfz.util.events", "$rfz.util.point", function(css, EVENTS, Point) {
-  function setPosition(slider, percent) {
-    var unit = -50 * percent;
-    css.setTranslate(slider[0], unit);
-  }
-
-  function toggleBackground(slider, show) {
-    slider.css("background-size", (show ? "64px 100%" : "0 0") + ", 63px 100%");
-  }
-
-  return {
-    restrict : "A",
-    replace : true,
-    scope : {
-      model : "=toggle"
-    },
-    templateUrl : "/partials/toggle.html",
-    link : function(scope, element, attr) {
-      var slider = angular.element(element[0].getElementsByClassName("toggle-slider")[0]);
-      var toggleBg = function() {
-        slider.unbind(EVENTS.TRANSITION_END, toggleBg);
-        toggleBackground(slider, scope.model)
-      };
-      if(!scope.model) {
-        setPosition(slider, 1);
-        toggleBackground(slider, scope.model);
-      }
-      var slidePercent = scope.model ? 0 : 1;
-
-      function slide(distance) {
-        var position;
-        if (scope.model) {
-          if (distance < 0) {
-            position = Math.max(distance, -50);
-          } else position = 0;
-        } else {
-          if (distance > 0) {
-            position = - 50 + Math.min(50, distance);
-          } else position = -50;
-        }
-        slidePercent = (position * -1) / 50
-        setPosition(slider, slidePercent);
-      }
-
-      var sliding = false;
-      var startPosition;
-
-      function pointerStart (e) {
-        sliding = false;
-        toggleBackground(slider, true);
-        startPosition = Point.fromEvent(e);
-        element.addClass("touch-start");
-      }
-
-      function pointerMove(e) {
-        e.preventDefault();
-        if(!sliding) {
-          sliding = true;
-          slider.addClass("no-transition");
-        }
-        slide(Point.fromEvent(e).x - startPosition.x);
-      }
-
-      function pointerEnd() {
-        slider.removeClass("no-transition");
-        if (sliding) {
-          if (slidePercent === 1) {
-            toggleBackground(slider, false);
-          } else {
-            slidePercent = Math.round(slidePercent);
-          }
-          scope.model = slidePercent === 0;
-          if (!scope.model) slider.bind(EVENTS.TRANSITION_END, toggleBg);
-
-          setPosition(slider, slidePercent);
-        } else {
-          if (scope.model) {
-            slider.bind(EVENTS.TRANSITION_END, toggleBg);
-            scope.model = false;
-            setPosition(slider, 1);
-          } else {
-            scope.model = true;
-            setPosition(slider, 0);
-          }
-        }
-      }
-
-      new EVENTS.PointerNested(element, {
-        start : pointerStart,
-        preMove : function() {
-          return true;
-        },
-        move : pointerMove,
-        end : pointerEnd
-      });
-    }
-    }
-}]);
-*/
-lib.directive("rfzToggle", ["$rfz.util.css", "$rfz.util.events", "$rfz.util.point", "$rfz.util.platform",
-                            function(css, events, Point, platform) {
+lib.directive("rfzToggle", ["$rfz.util.css", "$rfz.util.number", "$rfz.util.events", "$rfz.util.point", "$rfz.util.platform",
+                            function(css, numb, events, Point, platform) {
   return {
     restrict : "A",
     replace : true,
@@ -108,6 +9,9 @@ lib.directive("rfzToggle", ["$rfz.util.css", "$rfz.util.events", "$rfz.util.poin
     template : "<div class='rfz-toggle' ng-class=\"{on : model}\"><div class='rfz-toggle-handle'></div></div>",
     link : function(scope, element, attr) {
       var handle = element.children();
+      var startPosition;
+      var targetPoint;
+      var hasMoved;
 
       function getTargetPoint(start, firstChange) {
         var target = start.copy();
@@ -120,26 +24,22 @@ lib.directive("rfzToggle", ["$rfz.util.css", "$rfz.util.events", "$rfz.util.poin
         return target;
       }
 
-      var startPosition;
-      var targetPoint;
-      var hasMoved;
 
-      function pointerStart (e) {
+      function pointerStartIOS (e) {
         hasMoved = false;
-        startTime = new Date().getTime();
         startPosition = Point.fromEvent(e);
         element.addClass("pointer-start");
         targetPoint = getTargetPoint(startPosition, true);
       }
 
-      function pointerMove(e) {
+      function pointerMoveIOS(e) {
         var position = Point.fromEvent(e);
         if (scope.model) {
           if (position.x <= targetPoint.x) {
             hasMoved = true;
             scope.model = false;
             targetPoint = getTargetPoint(targetPoint, false);
-            scope.$digest();
+            scope.$apply();
           }
         } else {
           if (position.x >= targetPoint.x) {
@@ -151,29 +51,104 @@ lib.directive("rfzToggle", ["$rfz.util.css", "$rfz.util.events", "$rfz.util.poin
         }
       }
 
-      function pointerLost(e) {
+      function pointerLostIOS(e) {
         element.removeClass("pointer-start");
       }
 
 
-      function pointerEnd(e) {
-        pointerLost(e);
+      function pointerEndIOS(e) {
+        pointerLostIOS(e);
         if (!hasMoved) {
           scope.model = !scope.model;
-          scope.$digest();
+          scope.$apply();
         }
       }
 
       if (platform.os === platform.PLATFORMS.IOS) {
         new events.PointerNested(element, {
-          start : pointerStart,
+          start : pointerStartIOS,
           preMove : function() {
             return true;
           },
-          move : pointerMove,
-          end : pointerEnd,
-          lost : pointerLost
+          move : pointerMoveIOS,
+          end : pointerEndIOS,
+          lost : pointerLostIOS
 
+        });
+      } else {
+        handle.text(scope.model ? "ON" : "OFF");
+        var distance = 47;
+        var startModelValue;
+
+        function pointerStartAndroid(e) {
+          startModelValue = scope.model;
+          hasMoved = false;
+          startPosition = Point.fromEvent(e);
+          element.addClass("pointer-start");
+        }
+
+        function pointerPreMoveAndroid(e) {
+          if (hasMoved) {
+            return true;
+          } else {
+            var position = Point.fromEvent(e);
+            var delta = Point.difference(startPosition, position);
+            delta.abs();
+            if (delta.x === delta.y || delta.x > delta.y) {
+              return true;
+            } else {
+              return false;
+            }
+          }
+        }
+
+        function toggleAndroid() {
+          scope.model = !scope.model;
+          handle.text(scope.model ? "ON" : "OFF");
+          scope.$apply();
+        }
+
+        function pointerMoveAndroid(e) {
+          hasMoved = true;
+          var position = Point.fromEvent(e);
+          var delta = Point.difference(startPosition, position).x;
+
+          if (startModelValue) {
+            delta = -numb.clampNum(distance - delta, 0, distance);
+          } else {
+            delta = numb.clampNum(delta, -distance, 0);
+          }
+
+          css.setTranslate(handle[0], -delta, 0, 0);
+          if (scope.model) {
+            if (delta > -(distance / 2)) {
+              toggleAndroid();
+            }
+          } else {
+            if (delta < -(distance / 2)) {
+              toggleAndroid();
+            }
+          }
+        }
+
+        function pointerLostAndroid(e) {
+          element.removeClass("pointer-start");
+        }
+
+        function pointerEndAndroid(e) {
+          pointerLostAndroid(e);
+          if (!hasMoved) {
+            toggleAndroid();
+          }
+          css.setTransform(handle[0], "");
+        }
+
+        new events.PointerNested(element, {
+          start : pointerStartAndroid,
+          preMove : pointerPreMoveAndroid,
+          move : pointerMoveAndroid,
+          end : pointerEndAndroid,
+          lost : pointerLostAndroid
         });
       }
     }
