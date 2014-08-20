9b3c3e8484a6ab6bc8ab0132221283d3fdba754d
Marshall Brekka <marshallsmedia@gmail.com>
2014-08-20 16:32:56 -0700

    add the scroll view options to the controller on the pre link, so that we can determine information about the scroll view on child linking functions

--- src/directive/scroll-view.js
+++ src/directive/scroll-view.js
@@ -43,74 +43,81 @@ lib.directive("rfzScrollView", ["$rfz.util.scrollView", "$rfz.util.attrs", "$rfz
     scope : {
       currentPage : "="
     },
-    link : function(scope, element, attrs, ctrls) {
-      ctrls[0].element = element;
-      var ctrl = ctrls[1];
-      var el = element[0];
-      var content = element.children();
-      if (content.attr("rfz-scroll-view-body") === undefined) {
-        throw new Error("rfz-scroll-view requires an rfz-scroll-view-body");
-      } else {
-        content = content[0];
-      }
-      var scroll = new ScrollView({
-        container : el,
-        content : content,
-        canScrollX : attrsUtils.get(attrs, "canScrollX", true, attrsUtils.toBoolean),
-        canScrollY : attrsUtils.get(attrs, "canScrollY", true, attrsUtils.toBoolean),
-        showIndicatorX : attrsUtils.get(attrs, "showIndicatorX", true, attrsUtils.toBoolean),
-        showIndicatorY : attrsUtils.get(attrs, "showIndicatorY", true, attrsUtils.toBoolean),
-        pageSizeFactor : attrsUtils.get(attrs, "pageSizeFactor", 1, parseFloat),
-        pagingEnabled : attrsUtils.get(attrs, "pagingEnabled", false, attrsUtils.toBoolean),
-        autoPageHeight : attrsUtils.get(attrs, "autoPageHeight", false, attrsUtils.toBoolean)
-      });
-      if (ctrl && attrs.rfzScrollView) {
-        ctrl.registerScrollView(attrs.rfzScrollView, scroll);
-      }
-      ctrls[0].scrollView = scroll;
+    link : {
+      pre : function(scope, element, attrs, ctrls) {
+        ctrls[0].element = element;
+        ctrls[0].options = {
+          canScrollX : attrsUtils.get(attrs, "canScrollX", true, attrsUtils.toBoolean),
+          canScrollY : attrsUtils.get(attrs, "canScrollY", true, attrsUtils.toBoolean),
+          showIndicatorX : attrsUtils.get(attrs, "showIndicatorX", true, attrsUtils.toBoolean),
+          showIndicatorY : attrsUtils.get(attrs, "showIndicatorY", true, attrsUtils.toBoolean),
+          pageSizeFactor : attrsUtils.get(attrs, "pageSizeFactor", 1, parseFloat),
+          pagingEnabled : attrsUtils.get(attrs, "pagingEnabled", false, attrsUtils.toBoolean),
+          autoPageHeight : attrsUtils.get(attrs, "autoPageHeight", false, attrsUtils.toBoolean)
+        };
+      },
+      post : function(scope, element, attrs, ctrls) {
+        var el = element[0];
+        var content = element.children();
+        var ctrl = ctrls[1];
 
-      // adjust height on next tick to ensure that all content has
-      // been inserted according to angulars watch/digest lifecycle.
-      // Update: this might actually be due to a bug in the
-      // animation module in the RC version we are using.
-      // TODO: after updating version check if issues still exists.
-      setTimeout(function() {
-        if (scroll.autoPageHeight) {
-          scroll.adjustHeight();
+        if (content.attr("rfz-scroll-view-body") === undefined) {
+          throw new Error("rfz-scroll-view requires an rfz-scroll-view-body");
+        } else {
+          content = content[0];
         }
-        if (scope.currentPage) {
-          scroll.scrollToPage(scope.startPage);
-          scope.$watch("currentPage", function(newVal, oldVal) {
-            if (newVal !== oldVal) {
-              scroll.scrollToPage(newVal, true);
-            }
-          });
-        }
-        scroll.calculateMaxPoint();
-      }, 4);
 
-      var resize = _.throttle(function(defer) {
-        if (defer) {
-          _.defer(function() {
+        var scroll = new ScrollView(_.extend({
+          container : el,
+          content : content,
+        }, ctrls[0].options));
+        if (ctrl && attrs.rfzScrollView) {
+          ctrl.registerScrollView(attrs.rfzScrollView, scroll);
+        }
+        ctrls[0].scrollView = scroll;
+
+        // adjust height on next tick to ensure that all content has
+        // been inserted according to angulars watch/digest lifecycle.
+        // Update: this might actually be due to a bug in the
+        // animation module in the RC version we are using.
+        // TODO: after updating version check if issues still exists.
+        setTimeout(function() {
+          if (scroll.autoPageHeight) {
+            scroll.adjustHeight();
+          }
+          if (scope.currentPage) {
+            scroll.scrollToPage(scope.startPage);
+            scope.$watch("currentPage", function(newVal, oldVal) {
+              if (newVal !== oldVal) {
+                scroll.scrollToPage(newVal, true);
+              }
+            });
+          }
+          scroll.calculateMaxPoint();
+        }, 4);
+
+        var resize = _.throttle(function(defer) {
+          if (defer) {
+            _.defer(function() {
+              scroll.calculateMaxPoint();
+              scroll.snapPositionToBounds(true);
+            });
+          } else {
             scroll.calculateMaxPoint();
             scroll.snapPositionToBounds(true);
-          });
-        } else {
-          scroll.calculateMaxPoint();
-          scroll.snapPositionToBounds(true);
-        }
-      }, 100);
+          }
+        }, 100);
 
-      
-      scope.$watch(function() {
-        resize(true);
-        return 1;
-      }, function() {});
+        scope.$watch(function() {
+          resize(true);
+          return 1;
+        }, function() {});
 
-      events.bind(window, resize, "resize");
-      scope.$on("$destroy", function() {
-        events.unbind(window, resize, "resize");
-      });
+        events.bind(window, resize, "resize");
+        scope.$on("$destroy", function() {
+          events.unbind(window, resize, "resize");
+        });
+      }
     }
   }
 }]);
